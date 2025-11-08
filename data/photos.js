f1dda86b0ac2ec1964ea298b350178a1ca3fc74f
David Bilan <bilandavid07@gmail.com>
2025-11-08 18:53:21 +0100

    some more mockup photos

--- data/photos.js
+++ data/photos.js
@@ -1,24 +1,83 @@
 const photos = [
-  {
-    id: 1,
-    title: "Neuschwanstein",
-    imageUrl: "/images/IMG_9264.JPG",
-  },
-  {
-    id: 2,
-    title: "Tegernsee",
-    imageUrl: "/images/C7AC92DE-97BF-4CD3-827F-2865CF38F3EE.PNG",
-  },
-  {
-    id: 3,
-    title: "Alps",
-    imageUrl: "/images/9E4F17FF-55B9-4643-A2F4-7050369FB8CD.PNG",
-  },
-  {
-    id:4,
-    title: "Mountains",
-    imageUrl: "/images/mountains.jpg",
-  },
+    {
+        id: 1,
+        title: "Neuschwanstein",
+        imageUrl: "/images/IMG_9264.JPG",
+    },
+    {
+        id: 2,
+        title: "Tegernsee",
+        imageUrl: "/images/C7AC92DE-97BF-4CD3-827F-2865CF38F3EE.PNG",
+    },
+    {
+        id: 3,
+        title: "Alps",
+        imageUrl: "/images/9E4F17FF-55B9-4643-A2F4-7050369FB8CD.PNG",
+    },
+    {
+        id: 4,
+        title: "Mountains",
+        imageUrl: "/images/mountains.jpg",
+    },
+
+    {
+        id: 5,
+        title: "Riding",
+        imageUrl: "/images/6F0CDF14-37F2-4134-8646-748406395FDD.PNG",
+    },
+        {
+        id: 1,
+        title: "Neuschwanstein",
+        imageUrl: "/images/IMG_9264.JPG",
+    },
+    {
+        id: 2,
+        title: "Tegernsee",
+        imageUrl: "/images/C7AC92DE-97BF-4CD3-827F-2865CF38F3EE.PNG",
+    },
+    {
+        id: 3,
+        title: "Alps",
+        imageUrl: "/images/9E4F17FF-55B9-4643-A2F4-7050369FB8CD.PNG",
+    },
+    {
+        id: 4,
+        title: "Mountains",
+        imageUrl: "/images/mountains.jpg",
+    },
+
+    {
+        id: 5,
+        title: "Riding",
+        imageUrl: "/images/6F0CDF14-37F2-4134-8646-748406395FDD.PNG",
+    },
+        {
+        id: 1,
+        title: "Neuschwanstein",
+        imageUrl: "/images/IMG_9264.JPG",
+    },
+    {
+        id: 2,
+        title: "Tegernsee",
+        imageUrl: "/images/C7AC92DE-97BF-4CD3-827F-2865CF38F3EE.PNG",
+    },
+    {
+        id: 3,
+        title: "Alps",
+        imageUrl: "/images/9E4F17FF-55B9-4643-A2F4-7050369FB8CD.PNG",
+    },
+    {
+        id: 4,
+        title: "Mountains",
+        imageUrl: "/images/mountains.jpg",
+    },
+
+    {
+        id: 5,
+        title: "Riding",
+        imageUrl: "/images/6F0CDF14-37F2-4134-8646-748406395FDD.PNG",
+    },
+
 ];
 
 export default photos;

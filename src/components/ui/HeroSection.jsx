fbeb246711734357849abe22e3fdf36f9c124f59
David Bilan <bilandavid07@gmail.com>
2025-11-11 19:19:57 +0100

    Added appearance animation for the name

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -12,7 +12,7 @@ function HeroSection({ imageUrl, title }) {
                 style={{ backgroundImage: `url(${imageUrl})` }}
                 initial={{ scale: 1 }}
                 animate={{ scale: 1.08 }} // slightly zoom in
-                transition={{ duration: 5, ease: "easeOut" }} 
+                transition={{ duration: 5, ease: "easeOut" }}
             />
 
             {/* Dark overlay */}
@@ -20,11 +20,20 @@ function HeroSection({ imageUrl, title }) {
 
             {/* Centered text */}
             <div className="relative z-10 flex items-center justify-center h-full">
-                <h1 className="text-white/70 text-4xl sm:text-5xl md:text-6xl font-bold text-center px-4">
-                    {title}
-                </h1>
+                <motion.div
+                    initial={{ opacity: 0 }}            
+                    whileInView={{ opacity: 1 }}       
+                    transition={{ duration: 5, ease: "easeOut" }} 
+                >
+                    <h1 className="text-white/75 text-4xl sm:text-5xl md:text-6xl font-bold text-center px-4">
+                        {title}
+                    </h1>
+                </motion.div>
             </div>
-        </section>
+
+
+
+        </section >
 
 
     );

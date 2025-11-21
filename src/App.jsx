826bad44f1ed5528c1e0e9ed1392cfb56e3f3259
David Bilan <bilandavid07@gmail.com>
2025-11-21 23:48:09 +0100

    removed redundant component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 
 import Home from './pages/Home'
 
-import { Button } from "@/components/ui/button"
 
 
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
